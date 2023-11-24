b8f07a4810faf2f4c5e44822b7a06cb04a481c7a
Pouya Barati <pooya.dragongage@gmail.com>
2023-11-24 19:27:39 +0330

    Created update task page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,13 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield("tab_title")</title>
+@yield("styles")
 </head>
 <body>
+    @if (session()->has("success"))
+        <div>{{ session("success") }}</div>
+    @endif
+
     <h1>@yield("title")</h1>
     <div>
         @yield("content")
--- routes/web.php
+++ routes/web.php
@@ -2,91 +2,73 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Response;
+use Illuminate\Http\Request;
+use App\Models\Task;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get("/", function () { //Index of Project - List Tasks
 
-
-class Task
-{
-    public function __construct(
-        public int     $id,
-        public string  $title,
-        public string  $description,
-        public ?string $long_description,
-        public bool    $completed,
-        public string  $created_at,
-        public string  $updated_at
-    )
-    {
-    }
-}
-
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
-
-Route::get("/", function (){
-   return redirect()->route("tasks.index");
+    return redirect()->route("tasks.index");
 });
 
-Route::get('/tasks', function () use($tasks) {
-    return view('index', ["tasks" =>$tasks]);
+Route::get('/tasks', function () {    //Show Task
+
+    return view('index', [
+        "tasks" => Task::latest()
+            ->where("completed", true)->get()
+    ]);
 })->name("tasks.index");
 
+Route::view("/task/create", "create")  //Create Task
+    ->name("tasks.create");
 
-Route::get("task/{id}", function ($id) use ($tasks) {
-    $task = collect($tasks)->firstWhere("id", $id);
-    if(!$task){
-        abort(Response::HTTP_NOT_FOUND);
-    }
-    return view("show", ["task" =>$task]);
+Route::get("task/{id}/edit", function ($id) {   //Edit Form
+
+    return view("edit", ["task" => Task::findOrFail($id)]);
+})->name("tasks.edit");
+
+Route::get("task/{id}", function ($id) {  //Show Task Details
+
+    return view("show", ["task" => Task::findOrFail($id)]);
 })->name("tasks.show");
 
+Route::post("/tasks", function (Request $request) {  //Insert Task Into Database
+    $data = $request->validate([
+        "title" => "required|max:255",
+        "description" => "required",
+        "long_description" => "required"
+    ]);
 
-Route::fallback(function () {
-    return "404 Not Found!";
+    $task = new Task;
+    $task->title = $data["title"];
+    $task->description = $data["description"];
+    $task->long_description = $data["long_description"];
+
+    $task->save();
+
+    return redirect()->route("tasks.show", ["id" => $task->id])
+        ->with("success", "task created successfully!");
+
+})->name("tasks.store");
+
+Route::put("/tasks/{id}", function ($id, Request $request) {  //Insert Task Into Database
+    $data = $request->validate([
+        "title" => "required|max:255",
+        "description" => "required",
+        "long_description" => "required"
+    ]);
+
+    $task = Task::findOrFail($id);
+    $task->title = $data["title"];
+    $task->description = $data["description"];
+    $task->long_description = $data["long_description"];
+
+    $task->save();
+
+    return redirect()->route("tasks.show", ["id" => $task->id])
+        ->with("success", "task updated successfully!");
+
+})->name("tasks.update");
+
+Route::fallback(function () {  //404 Page
+    return (Response::HTTP_NOT_FOUND);
 });
