edac20e076b5312dbae29519a3ce37493e321f4f
pooya_trmnt <mac@Mintos-2.local>
2023-12-06 10:25:36 +0330

    Changed Back Cancel Button Logic & Changed Some Styles

--- resources/views/form.blade.php
+++ resources/views/form.blade.php
@@ -1,52 +1,56 @@
 @extends('layouts.app')
 
-@section('tab_title', isset($task) ? 'Edit Task : ' . $task -> title : 'Add Task' )
+@section('tab_title', isset($task) ? 'Edit Task : ' . $task->title : 'Add Task')
 
 @section('title', isset($task) ? 'Edit Task' : 'Add a new Task')
 
 @section('content')
 
-<form method="POST" action="{{ isset($task) ? route('tasks.update', ['task' => $task -> id]) : route('tasks.store') }}">
-    @csrf
-    @isset($task)
-        @method("PUT")
-    @endisset
-    <div style="mb-4">
-        <label for="title">Title</label>
-        <input type="text" name="title" id="title"
-        @class(["border-red-500" => $errors->has("title")]) value="{{ $task->title ?? old("title") }}">
-        @error("title")
-        <p class="error"> {{ $message }} </p>
-        @enderror
-    </div>
+    <form style="py-2" method="POST"
+        action="{{ isset($task) ? route('tasks.update', ['task' => $task->id]) : route('tasks.store') }}">
+        @csrf
+        @isset($task)
+            @method('PUT')
+        @endisset
+        <div style="mb-4 py-2">
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title" @class(['border-red-500' => $errors->has('title')])
+                value="{{ $task->title ?? old('title') }}">
+            @error('title')
+                <p class="error"> {{ $message }} </p>
+            @enderror
+        </div>
 
-    <div style="mb-4">
-        <label for="description">Description</label>
-        <textarea name="description" id="description"
-        @class(["border-red-500" => $errors->has("description")]) rows="5">{{ $task->description ?? old("description") }}</textarea>
-        @error("description")
-        <p class="error"> {{ $message }} </p>
-        @enderror
-    </div>
+        <div style="mb-4 py-2">
+            <label for="description">Description</label>
+            <textarea name="description" id="description" @class(['border-red-500' => $errors->has('description')]) rows="5">{{ $task->description ?? old('description') }}</textarea>
+            @error('description')
+                <p class="error"> {{ $message }} </p>
+            @enderror
+        </div>
 
-    <div style="mb-4">
-        <label for="long_description">Long Description</label>
-        <textarea name="long_description" id="long_description"
-        @class(["border-red-500" => $errors->has("long_description")]) rows="10">{{ $task->long_description ?? old("long_description") }}</textarea>
-        @error("long_description")
-        <p class="error"> {{ $message }} </p>
-        @enderror
-    </div>
+        <div style="mb-4 py-2">
+            <label for="long_description">Long Description</label>
+            <textarea name="long_description" id="long_description" @class(['border-red-500' => $errors->has('long_description')]) rows="10">{{ $task->long_description ?? old('long_description') }}</textarea>
+            @error('long_description')
+                <p class="error"> {{ $message }} </p>
+            @enderror
+        </div>
 
-    <div style="flex items-center gap-2">
-        <button class="btn" type="submit">@isset($task)
-            Update Task
-        @else
-            Add Task
-        @endisset</button>
+        <div style="flex items-center gap-2">
+            <button class="btn" type="submit">
+                @isset($task)
+                    Update Task
+                @else
+                    Add Task
+                @endisset
+            </button>
 
-        <a class="link" href="{{ URL::previous() }}">Cancel</a>
-    </div>
-</form>
+            <a class="link"
+                href="@if (URL::previous() == URL::current()) {{ route('tasks.index') }}
+            @else
+            {{ URL::previous() }} @endif">Cancel</a>
+        </div>
+    </form>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,11 +15,11 @@
     <style type="text/tailwindcss">
 
         .btn {
-            @apply rounded-md px-2 py-1 text-center font-medium text-slate-700 shadow-sm ring-1 ring-slate-700/10 hover:bg-slate-50
+            @apply bg-green-200 rounded-md px-2 py-1 mt-3 text-center font-medium shadow-sm ring-1 ring-green-700/10 hover:bg-slate-50
         }
 
         .link {
-            @apply font-medium text-gray-700 text-center rounded-md px-3 py-1 shadow-sm ring-1 ring-pink-500 bg-pink-50 hover:bg-pink-500
+            @apply font-medium text-center rounded-md mt-3 px-3 py-1 shadow-sm ring-1 ring-pink-500 bg-pink-50 hover:bg-pink-500
         }
 
         label {
@@ -43,36 +43,37 @@
 </head>
 
 <body class="bg-slate-50">
-<div class="container mx-auto mb-10 pb-10 max-w-lg">
-    <div class="px-4 py-4 bg-slate-100/90">
-        @if (session()->has('success'))
-        <div x-data="{ flash: true}">
-            <div x-show="flash">
-                <div class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700"
-            role="alert">
-            <strong class="font-bold">Success!</strong>
+    <div class="container mx-auto mb-10 pb-10 max-w-lg">
+        <div class="px-4 py-4 bg-slate-200/90">
+            @if (session()->has('success'))
+                <div x-data="{ flash: true }">
+                    <div x-show="flash">
+                        <div class="relative mb-10 rounded border border-green-400 bg-green-100 px-4 py-3 text-lg text-green-700"
+                            role="alert">
+                            <strong class="font-bold">Success!</strong>
+                            <div>
+                                {{ session('success') }}
+                            </div>
+
+                            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" @click="flash = false" stroke="currentColor"
+                                    class="h-6 w-6 cursor-pointer">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                                </svg>
+                            </span>
+
+                        </div>
+                    </div>
+                </div>
+            @endif
+
+            <h1 class="text-3xl mb-4">@yield('title')</h1>
             <div>
-                {{ session('success') }}
-            </div>
-
-            <span class="absolute top-0 bottom-0 right-0 px-4 py-3">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                    @click="flash = false" stroke="currentColor" class="h-6 w-6 cursor-pointer">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
-            </span>
-
-        </div>
+                @yield('content')
             </div>
         </div>
-    @endif
-
-    <h1 class="text-3xl mb-4">@yield('title')</h1>
-    <div>
-        @yield('content')
     </div>
-    </div>
-</div>
 </body>
 
 </html>
